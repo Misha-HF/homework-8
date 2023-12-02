bee83718015fee70d5069e3ee4fbf607130fcb8a
Misha-HF <misha.hub69@gmail.com>
2023-12-02 21:18:19 +0200

    third-commit

--- main.py
+++ main.py
@@ -11,25 +11,28 @@ def get_birthdays_per_week(users):
     # Оновлений словник для зберігання днів народження
     birthdays_per_week = {day: [] for day in days_of_week}
     for user in users:
-        name = user['name']  # імя користувача = відповідає імені кожного користувача
-        birthday = user['birthday']  # день народження користувача = відповідає дню народження кожного користувача
-        new_birthday = birthday.replace(year=now.year)  # найближчий день народження = в даних дня народження користувача змінюємо рік дня народження користувача на поточний
-        if new_birthday < now:  # якщо найближчий день народження менше поточної дати...
+        name = user['name']
+        birthday = user['birthday']
+        new_birthday = birthday.replace(year=now.year)
+        if new_birthday < now:
             new_birthday = new_birthday.replace(
-                year=now.year + 1)  # то найближчий день народження змінюємо шляхом заміни року народження на поточний рік народження для подальшого розрахунку
+                year=now.year + 1)
         # Перевіряємо, чи дата народження потрапляє в наступний тиждень
         if now <= new_birthday <= now + timedelta(
-                days=7):  # якщо оновлений показник дня народження знаходиться в інтервалі часу між поточною датою і датою + 7 днів то...
-            day_week = new_birthday.weekday()  # визначаємо на який день тижня припадає день народження
-            day_name = days_of_week[day_week] if day_week not in (5, 6) else 'Monday'
+                days=7):
+            day_week = new_birthday.weekday()
+            if day_week not in (5, 6):
+                day_name = days_of_week[day_week]
+            else:
+                'Monday'
             # Перевірка, чи день народження потрапляє на вихідний
-            if day_name in ['Saturday', 'Sunday']:  # в разі коли день тижня припадає на суботу чи неділю то...
-                day_name = 'Monday'  # Переносимо відображення циєї дати на понеділок
+            if day_name in ['Saturday', 'Sunday']:
+                day_name = 'Monday'
             birthdays_per_week[day_name].append(
-                name)  # в разі якщо відповідає попередній умові, добавляємо значення в словник...
+                name)
 
-    print(birthdays_per_week)  # друкуємо словник днів тижня з наявними/відсутніми днями народження...
-    return {key: value for key, value in birthdays_per_week.items() if value}  # повертаємо словник, видаляємо пусті списки
+    print(birthdays_per_week)
+    return {key: value for key, value in birthdays_per_week.items() if value}
 
 
 if __name__ == "__main__":
